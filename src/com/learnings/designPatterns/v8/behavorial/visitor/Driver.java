1d8cfb4cab2d2961152f09111689b494f648d249
Palash Kondekar <palash.kondekar@sap.com>
2020-08-15 20:46:35 +0530

    removed Shape interface

--- src/com/learnings/designPatterns/v8/behavorial/visitor/Driver.java
+++ src/com/learnings/designPatterns/v8/behavorial/visitor/Driver.java
@@ -4,6 +4,7 @@ import java.util.function.Consumer;
 
 import com.learnings.designPatterns.v8.model.Rectangle;
 import com.learnings.designPatterns.v8.model.Shape;
+import com.learnings.designPatterns.v8.model.Triangle;
 import com.learnings.designPatterns.v8.model.visitor.Body;
 import com.learnings.designPatterns.v8.model.visitor.Car;
 import com.learnings.designPatterns.v8.model.visitor.Engine;
@@ -15,13 +16,12 @@ public class Driver {
         Engine engine = new Engine();
         Car car = new Car();
         Body body = new Body();
-        
+
         // This consumer will consume the Visitor Builder object
         Consumer<VisitorBuilder<String>> consumer =
             Visitor.<Engine, String>forType(Engine.class).execute(e -> "Visited engine: " + e)
                 .forType(Car.class).execute(c -> "Visited car: " + c)
-                .forType(Body.class).execute(b -> "Visited Body: " + b)
-                .forType(Shape.class).execute(r -> "Visited rectangle: " + r);
+                .forType(Body.class).execute(b -> "Visited Body: " + b);
 
         // Here we pass the consumer of visitor builder to our visitor. 
         // This will allow the visitor to understnad what should be visited using the VisitorBuilder
